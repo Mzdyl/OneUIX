ed865380d035d8c3be4ffb83829a99c12589b1d2
SoClear <48470201+SoClear@users.noreply.github.com>
2026-03-15 13:09:25 +0800

    feat(SystemUI): Reboot System UI

--- app/src/main/java/io/github/soclear/oneuix/ui/category/DetailPaneSystemUI.kt
+++ app/src/main/java/io/github/soclear/oneuix/ui/category/DetailPaneSystemUI.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -52,6 +53,15 @@ fun DetailPaneSystemUI(
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+                runCatching { Runtime.getRuntime().exec("su -c killall com.android.systemui") }
+            }
+        ) {
+            Text(text = stringResource(R.string.rebootSystemUI))
+        }
         DividerText(R.string.status_bar)
         Column {
             var padding by remember {
