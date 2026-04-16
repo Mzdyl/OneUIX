65ea4bf7535b4a7b346525efbde4f389ec686cca
SoClear <48470201+SoClear@users.noreply.github.com>
2026-04-16 15:48:32 +0800

    build: update compileSdk and targetSdk to 37 and upgrade Java version to 25

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
-    id("com.android.library")
+    alias(libs.plugins.android.library)
 }
 
 android {
     namespace = "io.github.soclear.oneuix.stub"
-    compileSdk = 36
+    compileSdk {
+        version = release(37)
+    }
 
     defaultConfig {
         minSdk = 33
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_25
+        targetCompatibility = JavaVersion.VERSION_25
     }
 }
