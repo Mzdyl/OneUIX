4adebea56d00369dc6cd51c2718d44b16120551d
SoClear <48470201+SoClear@users.noreply.github.com>
2026-09-25 03:03:06 +0800

    chore: bump version to 1.9.0 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = providers.gradleProperty("oneuix.applicationId").get()
         minSdk = 33
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.8.0"
+        versionCode = 11
+        versionName = "1.9.0"
 
         ndk {
             // ABI过滤，只加载 arm 架构 64 位动态链接库
